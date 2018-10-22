753aa455fe4995832fbc054866c68059ab47e140
Tomek <niewiemnicnic@onet.pl>
2018-10-22 18:30:56 +0200

    first init - create form in html

---
+++ src/main/java/pl/tomek/piwowarstwo/Piwko/PiwkoApplication.java
@@ -0,0 +1,12 @@
+package pl.tomek.piwowarstwo.Piwko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiwkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiwkoApplication.class, args);
+	}
+}
